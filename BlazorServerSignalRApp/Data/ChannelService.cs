c511edb4916c0a568c1557221da26bb60ae26750
mtaku3 <m.taku3.1222@gmail.com>
2023-01-19 16:32:22 +0900

    feat: add channel feature

---
+++ BlazorServerSignalRApp/Data/ChannelService.cs
@@ -0,0 +1,7 @@
+using BlazorServerSignalRApp.Server.Hubs;
+
+namespace BlazorServerSignalRApp.Data;
+
+public class ChannelService {
+    public static List<string> Channels = new List<string>();
+}
--- BlazorServerSignalRApp/Hubs/Chathub.cs
+++ BlazorServerSignalRApp/Hubs/Chathub.cs
@@ -9,5 +9,18 @@ namespace BlazorServerSignalRApp.Server.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", message);
         }
+
+        public async Task SendMessageToChannel(string channelName, Message message) {
+            await Clients.Group(channelName).SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task JoinChannel(string name) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, name);
+            if (!ChannelService.Channels.Contains(name)) ChannelService.Channels.Add(name);
+        }
+
+        public async Task QuitChannel(string name) {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, name);
+        }
     }
 }
